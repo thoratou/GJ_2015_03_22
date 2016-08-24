845dbcc87ef7f2b2114c2a2bcaefc0af5f356207
tt.tranquang <tt.tranquang@gmail.com>
2016-08-24 15:05:31 +0200

    use new state wrapper for game init

--- main.go
+++ main.go
@@ -1,23 +1,8 @@
 package main
 
-import "github.com/gopherjs/gopherjs/js"
-
-type JsWorld struct {
-	*js.Object
-	World *World
-}
+import "github.com/thoratou/go-phaser/generated/phaser"
 
 func main() {
-	world := NewWorld()
-	jsWorld := &JsWorld{
-		Object: js.MakeWrapper(world),
-		World:  world,
-	}
-
-	jsWorld.Set("preload", jsWorld.World.Preload)
-	jsWorld.Set("create", jsWorld.World.Create)
-	jsWorld.Set("update", jsWorld.World.Update)
-
-	Game().State().Add("world", jsWorld)
+	Game().State().Add("world", phaser.WrapState(NewWorld()))
 	Game().State().StartI("world")
 }
